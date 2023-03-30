f03dcc3ecbc166689a62ec4f818aa1359dc78339
Facu Mediotte <facumediotte@gmail.com>
2023-03-30 19:23:27 -0300

    Flow first constructor

--- src/main/kotlin/com/kotlin/async/Flow.kt
+++ src/main/kotlin/com/kotlin/async/Flow.kt
@@ -1,7 +1,43 @@
 package com.kotlin.async
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+
 fun main() {
-    show()
+    /*//show()
+    runBlocking {
+        runAsynchronous().forEach { println(it) }
+    }*/
+
+    /*runBlocking {
+        launch {
+            for (j in 1..3) {
+                println("No estoy bloqueado $j")
+                delay(1000)
+            }
+        }
+        firstFlow().collect { println(it) }
+    }
+
+    runBlocking {
+        println("Llamando Flow...")
+        val flow = firstFlow();
+        println("Collect...")
+        flow.collect { println(it) }
+        println("Collect again...")
+        flow.collect { println(it) }
+    }*/
+
+    //Cancel flow
+    runBlocking {
+        withTimeoutOrNull(2500) {
+            firstFlow().collect { println(it) }
+        }
+        println("Finalizado")
+    }
 }
 
 fun show() {
@@ -12,8 +48,26 @@ fun show() {
 fun listar(): List<Int> = listOf(3, 78, 90)
 
 fun secuencia(): Sequence<Int> = sequence {
-    for(i in 1..3) {
+    for (i in 1..3) {
         Thread.sleep(1000)
         yield(i)
+    }
+}
+
+suspend fun runAsynchronous(): List<Int> {
+    return runBlocking {
+        delay(1000)
+        return@runBlocking listOf(1, 2, 3)
+    }
+}
+
+//Flow: call asynchronous data stream, se ejecuta de manera sequencial en la misma coroutine. Emite valores secuencialmente.
+//Parecidos a los streams de Java, no nos tenemos que preocupar por los memory leaks.
+//Necesita siempre un constructor
+
+fun firstFlow(): Flow<Int> = flow {
+    for (i in 1..3) {
+        delay(1000)
+        emit(i)
     }
 }
